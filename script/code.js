30e4df4dd5018f939988aab83ca3b576956597bc
LeahBasson <leahbasson54@gmail.com>
2024-05-17 15:00:55 +0200

    Update

--- script/code.js
+++ script/code.js
@@ -1,10 +1,27 @@
+//Using html & js (function)
+// function celsiusToFahrenheit(){
+//     let celsius = +document.querySelector('[Celsius]').value;
+//     document.querySelector('[Fahrenheit]').value = ((celsius*1.8)+32).toFixed(2);
+// }
 
-function celsiusToFahrenheit(){
+// function fahrenheitToCelsius(){
+//     let Fahrenheit = +document.querySelector('[Fahrenheit]').value;
+//     document.querySelector('[Celsius]').value = ((Fahrenheit-32)/1.8).toFixed(2);
+// }
+
+
+// Using only js (method)
+document.querySelector('[Celsius]').addEventListener("keyup", ()=>{
     let celsius = +document.querySelector('[Celsius]').value;
     document.querySelector('[Fahrenheit]').value = ((celsius*1.8)+32).toFixed(2);
-}
+})
 
-function fahrenheitToCelsius(){
+document.querySelector('[Fahrenheit]').addEventListener("keyup", ()=>{
     let Fahrenheit = +document.querySelector('[Fahrenheit]').value;
     document.querySelector('[Celsius]').value = ((Fahrenheit-32)/1.8).toFixed(2);
-}
+})
+
+
+
+
+
